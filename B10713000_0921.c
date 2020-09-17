68614ad28bd28269ecbac5273d076929c8fe32b4
drkeithwu <drkeithwu@yahoo.com.tw>
2020-09-17 14:57:58 +0800

    0917

---
+++ B10713000_0921.c
@@ -0,0 +1,20 @@
+// ..........
+// 109.9.17
+// v 1.0
+main()
+{  system("title §d¦J®ֹ");
+      system("mode 30,10");
+	  system("color e5");    
+      printf("  ¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p\n");
+      printf("  ¢pשÝשששששששששששששששששששששששששששששששששששששששß¢p\n");  
+      printf("  ¢pשר                   שר¢p\n");             
+      printf("  ¢pשר    ¦­     ¦w      שר¢p\n");       
+      printf("  ¢pשר B10913538 §d¦J®ֹ  שר¢p\n");
+      printf("  ¢pשר      ֵw×ן¥תֱ{     שר¢p\n");  
+      printf("  ¢pשר                   שר¢p\n"); 
+      printf("  ¢pשדשששששששששששששששששששששששששששששששששששששששו¢p\n"); 
+      printf("  ¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p¢p\n");
+
+
+      getch();
+}
